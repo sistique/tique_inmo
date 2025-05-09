ffed715d125898902fbb6db047a5a805cea200bf
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-05-08 18:08:37 -0600

    Se ajusta elemento de estilo login

--- vendor/gamboa.martin/template_1/views/adm_session/login.php
+++ vendor/gamboa.martin/template_1/views/adm_session/login.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="top-title">
+            <div class="top-title-titulo">
                 <ul class="breadcrumb">
                     <li class="item"><a href="index.php"> Inicio </a></li>
                     <li class="item"> Login</li>
