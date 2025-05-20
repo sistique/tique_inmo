f6ca1837479438d716f63d75850ba6abc673d024
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-05-20 14:27:20 -0600

    Se ajsuta elemento de overwrite

--- test_dropbox.php
+++ test_dropbox.php
@@ -12,7 +12,7 @@ use gamboamartin\inmuebles\models\_dropbox;
 $con = new conexion();
 $link = conexion::$link;
 
-$nombre_doc = 'hola.pdf';
+/*$nombre_doc = 'hola.pdf';
 $name = 'archivos/doc_documento/9.858556784382.pdf';
 
 $guarda = (new _dropbox(link: $link))->upload(archivo_drop: $nombre_doc, archivo_local: $name);
@@ -21,18 +21,24 @@ if (errores::$error) {
 }
 
 $nombre_local = 'descarga.pdf';
-$guarda = (new _dropbox(link: $link))->download(dropbox_id: 'id:zQQGljOYR2oAAAAAAAAHZQ',archivo_local: $nombre_local);
+$guarda = (new _dropbox(link: $link))->download(dropbox_id: 'id:zQQGljOYR2oAAAAAAAAHdw',archivo_local: $nombre_local);
 if (errores::$error) {
     return $this->error->error('Error al guardar archivo', $guarda);
 }
 
-$guarda = (new _dropbox(link: $link))->preview(dropbox_id: 'id:zQQGljOYR2oAAAAAAAAHZQ');
+$guarda = (new _dropbox(link: $link))->preview(dropbox_id: 'id:zQQGljOYR2oAAAAAAAAHdw');
 if (errores::$error) {
     return $this->error->error('Error al guardar archivo', $guarda);
 }
 
-$guarda = (new _dropbox(link: $link))->delete(dropbox_id: 'id:zQQGljOYR2oAAAAAAAAHZQ');
+$guarda = (new _dropbox(link: $link))->delete(dropbox_id: 'id:zQQGljOYR2oAAAAAAAAHdw');
 if (errores::$error) {
     return $this->error->error('Error al guardar archivo', $guarda);
 }
+
+$nombre_local = 'archivos/doc_documento/21.718030211876.pdf';
+$guarda = (new _dropbox(link: $link))->overwrite(dropbox_id: 'id:zQQGljOYR2oAAAAAAAAHdw',archivo_local: $nombre_local);
+if (errores::$error) {
+    return $this->error->error('Error al guardar archivo', $guarda);
+}*/
 exit;
