30863d6dff4086885b4bee44cd304fc881ffe970
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-05-29 11:01:12 -0600

    Se ajsuta view inicial de docs

--- views/inm_doc_comprador/vista_previa.php
+++ views/inm_doc_comprador/vista_previa.php
@@ -4,7 +4,7 @@
 <main class="main section-color-primary">
     <div >
 
-        <div class="row">
+        <div class="row ">
 
             <div class="col-lg-12">
 
@@ -15,24 +15,29 @@
                     <?php include (new views())->ruta_templates."mensajes.php"; ?>
 
 
-                    <?php echo $controlador->inputs->com_tipo_cliente_descripcion; ?>
+                    <?php echo $controlador->inputs->com_tipo_comprador_descripcion; ?>
                     <?php echo $controlador->inputs->nss; ?>
                     <?php echo $controlador->inputs->curp; ?>
                     <?php echo $controlador->inputs->rfc; ?>
                     <?php echo $controlador->inputs->apellido_paterno; ?>
                     <?php echo $controlador->inputs->apellido_materno; ?>
                     <?php echo $controlador->inputs->nombre; ?>
-                    <?php echo $controlador->button_inm_doc_comprador_descarga; ?>
+                    <div class="control-group col-sm-12" style="margin-top: 10px;">
+                        <div class="controls">
+                            <?php echo $controlador->button_inm_doc_comprador_descarga; ?>
+                            <br>
+                        </div>
+                    </div>
 
                 </div>
             </div>
 
-            <div class="col-lg-12">
+            <div class="col-lg-12 ">
                 <div class="row">
                     <br>
                     <div class="col-md-12">
                         <?php echo $controlador->inputs->inm_doc_comprador_id; ?>
-                        <iframe class="col-md-12" height="600px" src="<?php echo $controlador->ruta_doc; ?>"></iframe>
+                        <iframe class="col-md-12 view" height="100%" src="<?php echo $controlador->ruta_doc; ?>"></iframe>
                     </div>
                 </div>
                 <br>
