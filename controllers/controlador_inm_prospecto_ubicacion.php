eb4100f89a2517fb1629b35742c7ec67a4243861
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-05-29 13:13:35 -0600

    Se ajusta elemento de integra relacion para reajustar prospecto

--- controllers/controlador_inm_prospecto_ubicacion.php
+++ controllers/controlador_inm_prospecto_ubicacion.php
@@ -615,19 +615,45 @@ class controlador_inm_prospecto_ubicacion extends _ctl_formato
 
         $this->link->beginTransaction();
 
-        $inm_prospecto_ubicacion = (new inm_prospecto_ubicacion($this->link))->registro(registro_id: $this->registro_id);
+        $modelo_inm_prospecto_ubicacion = new inm_prospecto_ubicacion($this->link);
+        $inm_prospecto_ubicacion = $modelo_inm_prospecto_ubicacion->registro(registro_id: $this->registro_id);
         if (errores::$error) {
             $this->link->rollBack();
             return $this->retorno_error(mensaje: 'Error al insertar datos', data: $inm_prospecto_ubicacion, header: $header, ws: $ws);
         }
 
         $con_rel_agente = new com_rel_agente($this->link);
-        $registro['com_agente_id'] = $_POST['com_agente_id'];
-        $registro['com_prospecto_id'] = $inm_prospecto_ubicacion['com_prospecto_id'];
-        $result = $con_rel_agente->alta_registro(registro: $registro);
+
+        $filtro['com_prospecto.id'] = $inm_prospecto_ubicacion['com_prospecto_id'];
+        $r_com_rel = $con_rel_agente->filtro_and(filtro: $filtro);
         if (errores::$error) {
             $this->link->rollBack();
-            return $this->retorno_error(mensaje: 'Error al insertar datos', data: $result, header: $header, ws: $ws);
+            return $this->retorno_error(mensaje: 'Error al insertar datos', data: $r_com_rel, header: $header, ws: $ws);
+        }
+
+        $registro['com_agente_id'] = $_POST['com_agente_id'];
+        $registro['com_prospecto_id'] = $inm_prospecto_ubicacion['com_prospecto_id'];
+
+        if($r_com_rel->n_registros > 0){
+            $result = $con_rel_agente->modifica_bd(registro: $registro, id: $r_com_rel->registros[0]['com_rel_agente_id']);
+            if (errores::$error) {
+                $this->link->rollBack();
+                return $this->retorno_error(mensaje: 'Error al insertar datos', data: $result, header: $header, ws: $ws);
+            }
+        }else{
+            $result = $con_rel_agente->alta_registro(registro: $registro);
+            if (errores::$error) {
+                $this->link->rollBack();
+                return $this->retorno_error(mensaje: 'Error al insertar datos', data: $result, header: $header, ws: $ws);
+            }
+        }
+
+        $registros_prosp['com_agente_id'] = $_POST['com_agente_id'];
+        $r_modifica = (new com_prospecto(link: $this->link))->modifica_bd(registro: $registros_prosp,
+            id: $inm_prospecto_ubicacion['com_prospecto_id']);
+        if (errores::$error) {
+            $this->link->rollBack();
+            return $this->retorno_error(mensaje: 'Error al insertar datos', data: $r_modifica, header: $header, ws: $ws);
         }
 
         $this->link->commit();
