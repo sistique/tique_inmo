7a536e12a46a72a34314bf972f4693479564c78e
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-06-09 17:22:25 -0600

    Se ajusta elemento de proceso ubicacion

--- js/inm_ubicacion/proceso_ubicacion.js
+++ js/inm_ubicacion/proceso_ubicacion.js
@@ -179,3 +179,31 @@ modal.addEventListener('click', function (event) {
 
 /***** Documentos *****/
 
+const columns_tipos_documentos = [
+    {
+        title: "Tipo documento",
+        data: "doc_tipo_documento_descripcion"
+    },
+    {
+        title: "Descarga",
+        data: "descarga"
+    },
+    {
+        title: "Vista previa",
+        data: "vista_previa"
+    },
+    {
+        title: "ZIP",
+        data: "descarga_zip"
+    },
+    {
+        title: "Elimina",
+        data: "elimina_bd"
+    }
+];
+
+const options = {paging: false, info: false, searching: false}
+
+const table_tipos_documentos = table('inm_ubicacion', columns_tipos_documentos, [], [], function () {
+    }, true,
+    "tipos_documentos", {registro_id: registro_id}, options);
