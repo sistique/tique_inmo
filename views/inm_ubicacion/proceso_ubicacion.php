a7b3bff706f85d5911fd3712f600be12db7609cf
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-06-10 11:43:02 -0600

    Se ajusta elemento de vista rtapa

--- views/inm_ubicacion/proceso_ubicacion.php
+++ views/inm_ubicacion/proceso_ubicacion.php
@@ -126,7 +126,6 @@
                                 </div>
                                 <div class="conten" id="cpestanaubicacion3">
                                     <form enctype="multipart/form-data" method="post" action="<?php echo $controlador->link_fotografia_bd; ?>" class="form-additional">
-                                        <?php include (new views())->ruta_templates."head/subtitulo.php"; ?>
                                         <?php foreach ($controlador->fotos as $registro){ ?>
                                             <div class="col-lg-12 contorno"  data-doc_tipo_documento_id ="<?php echo $registro['doc_tipo_documento_id']; ?>" >
                                                 <?php echo $registro['input']; ?>
@@ -146,7 +145,57 @@
                                     </form>
                                 </div>
                                 <div class="conten" id="cpestanaubicacion4">
-                                    3
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
+                                                <form method="post" action="<?php echo $controlador->link_alta_bitacora; ?>" class="form-additional">
+                                                    <?php echo $controlador->inputs->inm_ubicacion_id; ?>
+                                                    <?php echo $controlador->inputs->inm_status_ubicacion_id; ?>
+                                                    <?php echo $controlador->inputs->fecha; ?>
+                                                    <?php echo $controlador->inputs->observaciones; ?>
+
+                                                    <input type='hidden' name='seccion_retorno' value='inm_ubicacion'>
+                                                    <input type='hidden' name='btn_action_next' value='etapa'>
+                                                    <input type='hidden' name='id_retorno' value='<?php echo $controlador->registro_id; ?>'>
+
+                                                    <div class="controls">
+                                                        <button type="submit" class="btn btn-success" value="etapa" name="btn_action_next">Alta</button><br>
+                                                    </div>
+                                                </form>
+
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="widget widget-box box-container widget-mylistings">
+                                                <table class="table table-striped">
+                                                    <thead>
+                                                    <tr>
+                                                        <th>Id</th>
+                                                        <th>Etapa</th>
+                                                        <th>Fecha</th>
+                                                        <th>Observaciones</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    <?php
+                                                    foreach ($controlador->etapas as $etapa){
+                                                        ?>
+                                                        <tr>
+                                                            <td><?php echo $etapa['inm_bitacora_status_ubicacion_id'] ?></td>
+                                                            <td><?php echo $etapa['inm_status_ubicacion_descripcion'] ?></td>
+                                                            <td><?php echo $etapa['inm_bitacora_status_ubicacion_fecha_status'] ?></td>
+                                                            <td><?php echo $etapa['inm_bitacora_status_ubicacion_observaciones'] ?></td>
+                                                        </tr>
+                                                    <?php } ?>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
