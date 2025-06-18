94c6ab991696030ae07c1b14e740246df59bc2fe
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-06-18 16:08:52 -0600

    Se ajusta elemento de proceso prospecto

--- views/inm_prospecto/proceso_prospecto.php
+++ views/inm_prospecto/proceso_prospecto.php
@@ -35,10 +35,6 @@
                                     <div class="row">
                                         <div class="col-lg-12">
                                             <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
-                                                <?php include (new views())->ruta_templates . "head/subtitulo.php"; ?>
-                                                <?php include (new views())->ruta_templates . "mensajes.php"; ?>
-                                                <?php echo $controlador->buttons['btn_collapse_all']; ?>
-
                                                 <form method="post" action="<?php echo $controlador->link_modifica_bd; ?>" class="form-additional"
                                                       enctype="multipart/form-data">
 
@@ -320,9 +316,10 @@
                                                     <?php echo $controlador->inputs->razon_social; ?>
                                                     <?php echo $controlador->inputs->com_agente_id; ?>
 
-                                                    <div class="controls">
-                                                        <button type="submit" class="btn btn-success" value="correo" name="btn_action_next">Alta</button><br>
-                                                    </div>
+                                                    <?php echo $controlador->inputs->btn_action_next; ?>
+                                                    <?php echo $controlador->inputs->id_retorno; ?>
+                                                    <?php echo $controlador->inputs->seccion_retorno; ?>
+                                                    <?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
                                                 </form>
 
                                             </div>
@@ -359,26 +356,18 @@
                                 <div class="conten" id="cpestanaprospecto4">
                                     <div class="row">
                                         <div class="col-lg-12">
-                                            <?php include (new views())->ruta_templates."head/title.php"; ?>
-
-                                            <?php include (new views())->ruta_templates."mensajes.php"; ?>
-
                                             <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
                                                 <form method="post" action="<?php echo $controlador->link_alta_bitacora; ?>" class="form-additional">
-                                                    <?php include (new views())->ruta_templates."head/subtitulo.php"; ?>
-
                                                     <?php echo $controlador->inputs->inm_prospecto_id; ?>
                                                     <?php echo $controlador->inputs->inm_status_prospecto_id; ?>
                                                     <?php echo $controlador->inputs->fecha; ?>
                                                     <?php echo $controlador->inputs->observaciones; ?>
 
-                                                    <input type='hidden' name='seccion_retorno' value='inm_prospecto'>
-                                                    <input type='hidden' name='btn_action_next' value='etapa'>
-                                                    <input type='hidden' name='id_retorno' value='<?php echo $controlador->registro_id; ?>'>
+                                                    <?php echo $controlador->inputs->btn_action_next; ?>
+                                                    <?php echo $controlador->inputs->id_retorno; ?>
+                                                    <?php echo $controlador->inputs->seccion_retorno; ?>
 
-                                                    <div class="controls">
-                                                        <button type="submit" class="btn btn-success" value="etapa" name="btn_action_next">Alta</button><br>
-                                                    </div>
+                                                    <?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
                                                 </form>
 
                                             </div>
