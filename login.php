bca37ad1f582dce74bfea18625696f40de5510f6
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-07-28 22:52:20 -0600

    Se ajusta elemento de login

--- login.php
+++ login.php
@@ -3,8 +3,6 @@
 require "init.php";
 require 'vendor/autoload.php';
 
-$_SESSION['usuario_id'] = 2;
-
 use base\conexion;
 use gamboamartin\errores\errores;
 use gamboamartin\inmuebles\models\inm_empleado;
@@ -14,26 +12,26 @@ header("Content-Type: application/json");
 $con = new conexion();
 $link = conexion::$link;
 
-$data = json_decode(file_get_contents("php://input"), true);
-
-if (!$data) {
-    http_response_code(400);
-    echo json_encode(["error" => "Datos no validos"]);
-    exit;
-}
+$usuario = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
 
 try {
     $modelo_empleado = new inm_empleado(link: $link);
-
-    $usuario = $modelo_empleado->valida_usuario(password: $data['password'], usuario: $data['usuario']);
+    $r_usuario = $modelo_empleado->valida_usuario(password: $password, usuario: $usuario);
     if (errores::$error) {
-        http_response_code(500);
-        echo json_encode(["error" => $usuario]);
+        http_response_code(401);
+        echo json_encode(["success" => false, "error" => $r_usuario]);
+        exit;
     }
-    
-    echo json_encode(["status" => $usuario]);
+
+    echo json_encode([
+        "success" => true,
+        "inm_empleado_id" => $r_usuario['inm_empleado_id']
+    ]);
+    exit;
 
 } catch (PDOException $e) {
     http_response_code(500);
-    echo json_encode(["error" => $e]);
+    echo json_encode(["success" => false, "error" => $e->getMessage()]);
+    exit;
 }
