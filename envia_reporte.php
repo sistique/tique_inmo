f52af9b92d3785668a71404de77c341bc0774be4
sistique <sistemas.hdez1983@gmail.com>
2025-08-06 11:58:50 -0600

    Se ajusta elemento de periodo asistencia

--- envia_reporte.php
+++ envia_reporte.php
@@ -18,6 +18,7 @@ $modelo_inm_periodo_asistencia = new inm_periodo_asistencia(link: $link);
 
 $filtro['inm_periodo_asistencia.fecha_envio'] = date('Y-m-d');
 $filtro['inm_periodo_asistencia.enviado'] = 'inactivo';
+$filtro['inm_periodo_asistencia.status'] = 'activo';
 $r_periodos = $modelo_inm_periodo_asistencia->filtro_and(filtro: $filtro);
 if (errores::$error) {
     return $modelo_inm_periodo_asistencia->error->error('Error al guardar archivo', $r_periodos);
