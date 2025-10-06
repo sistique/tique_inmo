751f67fbe95ae1fc31dace0ae748cab571a2e1a2
sistique <sistemas.hdez1983@gmail.com>
2025-10-06 10:42:35 -0600

    Se ajusta elemento de impuesto para trasladado

--- views/inm_factura_compra/productos_factura.php
+++ views/inm_factura_compra/productos_factura.php
@@ -247,7 +247,7 @@ echo "<style>
                                 <td><?php echo $detalle['inm_detalle_factura_compra_cantidad'] ?></td>
                                 <td><?php echo $detalle['inm_detalle_factura_compra_valor_unitario'] ?></td>
                                 <td><?php echo $detalle['inm_detalle_factura_compra_subtotal'] ?></td>
-                                <td><?php echo $detalle['inm_detalle_factura_compra_iva'] ?></td>
+                                <td><?php echo $detalle['inm_detalle_factura_compra_trasladado'] ?></td>
                                 <td><?php echo $detalle['inm_detalle_factura_compra_total'] ?></td>
                                 <td><?php echo $detalle['elimina_bd'] ?></td>
                             </tr>
