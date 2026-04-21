743a135809d7df8b33b4da6499ede42cd019d855
sistique <sistemas.hdez1983@gmail.com>
2026-04-21 12:44:33 -0600

    Se ajsuta emision de recurso

--- controllers/controlador_inm_ubicacion.php
+++ controllers/controlador_inm_ubicacion.php
@@ -2441,7 +2441,7 @@ class controlador_inm_ubicacion extends _ctl_base {
             }
             
             $filtro_exi['inm_ubicacion.id'] = $this->registro_id;
-            $filtro_exi['inm_status_ubicacion.id'] = 8;
+            $filtro_exi['inm_status_ubicacion.id'] = 4;
             $existe = (new inm_bitacora_status_ubicacion(link: $this->link))->existe(filtro: $filtro_exi);
             if (errores::$error) {
                 $this->link->rollBack();
@@ -2452,7 +2452,7 @@ class controlador_inm_ubicacion extends _ctl_base {
             if (!$existe) {
                 $registro = array();
                 $registro['inm_ubicacion_id'] = $this->registro_id;
-                $registro['inm_status_ubicacion_id'] = 8;
+                $registro['inm_status_ubicacion_id'] = 4;
                 $registro['fecha_status'] = date('Y-m-d\TH:i:s');
                 $r_inm_bitacora_status_ubicacion = (new inm_bitacora_status_ubicacion(link: $this->link))->alta_registro(
                     registro: $registro);
