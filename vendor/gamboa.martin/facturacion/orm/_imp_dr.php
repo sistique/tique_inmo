cdcb8b46b7cc7f36ec12dbf935bdd84243ebce2c
sistique <sistemas.hdez1983@gmail.com>
2026-07-07 14:50:58 -0600

    Se ajusta elemento de factor

--- vendor/gamboa.martin/facturacion/orm/_imp_dr.php
+++ vendor/gamboa.martin/facturacion/orm/_imp_dr.php
@@ -48,7 +48,7 @@ class _imp_dr extends _modelo_parent{
                 return $this->error->error(mensaje: 'Error al obtener fc_rows', data: $fc_rows);
             }
         }
-        //print_r($fc_rows);exit;
+        
         if($this->tabla === 'fc_retencion_dr') {
             $fc_rows = (new _calculo_imps())->retenciones(modelo_retencion: $modelo_retencion,
                 key_filtro_id: 'fc_factura_id', registro_id: $fc_row_dr['fc_factura_id']);
